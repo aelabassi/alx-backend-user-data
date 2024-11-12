3c065844a89041a786fc30da1c4f0c6be0f2d240
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-12 15:03:17 +0100

    Basic - Base64 #6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,12 @@ from typing import TypeVar
 class BasicAuth(Auth):
     """ BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract_base64_authorization_header method
+        """
+        if authorization_header is None or \
+           type(authorization_header) is not str or \
+           not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
