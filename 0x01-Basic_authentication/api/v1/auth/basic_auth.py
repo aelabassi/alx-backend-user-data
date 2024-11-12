2022b5f140329461f1873e0a2f1e431b2d571e61
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-12 15:27:26 +0100

    Basic - Base64 - User object #10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -44,3 +45,21 @@ class BasicAuth(Auth):
            ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str)\
+            -> User:
+        """ user_object_from_credentials method """
+        if user_email is None or user_pwd is None or\
+            not isinstance(user_email, str)\
+                or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
