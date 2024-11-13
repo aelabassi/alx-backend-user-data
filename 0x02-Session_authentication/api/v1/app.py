4b4edf6d07f3b5db26ad82f033271657d32e0d49
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-13 16:40:29 +0100

    Session authentication: before request #5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,7 +35,7 @@ def before_request():
                                         '/api/v1/unauthorized/',
                                         '/api/v1/forbidden/'],
                                         '/api/v1/auth_session/login/'):
-        if not auth.authorization_header(request)\
+        if auth.authorization_header(request) is None\
                 and auth.session_cookie(request) is None:
             abort(401)
         current_user = auth.current_user(request)
