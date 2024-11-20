813bc44be9dcb02a9d47ccb5655629715b0a629d
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-20 14:42:37 +0100

    chore(app:flask) #14 logour route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ app module """
 from auth import Auth
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -37,6 +37,17 @@ def login():
             response.set_cookie("session_id", session_id)
             return response
     abort(401)
+
+
+@app.route('/sessions', methods=["DELETE"], strict_slashes=False)
+def logout():
+    """ DELETE /sessions route """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    abort(403)
 
 
 if __name__ == "__main__":
