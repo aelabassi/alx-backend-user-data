943c8f582d54a64103873aea8cf2e4f187b018fc
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-20 11:28:57 +0100

    User model (#0)

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,8 +9,8 @@ Base = declarative_base()
 class User(Base):
     """ User class """
     __tablename__ = 'users'
-    id: Column(Integer, primary_key=True)
-    email: Column(String(128), nullable=False)
-    hashed_password: Column(String(128), nullable=False)
-    session_id: Column(String(128), nullable=True)
-    reset_token: Column(String(128), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(128), nullable=False)
+    hashed_password = Column(String(128), nullable=False)
+    session_id = Column(String(128), nullable=True)
+    reset_token = Column(String(128), nullable=True)
