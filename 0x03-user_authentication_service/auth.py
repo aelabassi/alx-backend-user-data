46ee1181f0bd549c7bb80b150a1626ee5ddf7f64
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-20 13:41:06 +0100

    chore(auth) #9 generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,6 +12,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid(self) -> str:
+    """ Generate a UUID """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -35,7 +40,3 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid(self) -> str:
-        """ Generate a UUID """
-        return str(uuid4())
