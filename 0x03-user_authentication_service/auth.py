c7141a7700564864ca5bebc14b55a5a3faa10e59
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-20 14:36:39 +0100

    chore(auth) #13 destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ updates the user's session id to None"""
+        self._db.update_user(user_id, session_id=None)
+        return None
